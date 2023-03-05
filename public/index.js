6417e86878018499f6b078617e9cee5ebe9e69f2
Azat-Bilalov <52991059+Azat-Bilalov@users.noreply.github.com>
2023-03-05 23:14:43 +0300

    промежуточный вариант (д/з 2)

--- public/index.js
+++ public/index.js
@@ -1,14 +1,319 @@
-const header = document.querySelector('.header');
-const add_btn = document.getElementById('add_ann');
-const anns = document.getElementById('anns');
+const rootElement = document.getElementById('root');
+const headerElement = document.createElement('header');
+const contentElement = document.createElement('main');
+rootElement.appendChild(headerElement);
+rootElement.appendChild(contentElement)
 
-console.log(header);
+const config = {
+    board: {
+        name: 'Объявления',
+        href: '/board',
+        render: renderBoard,
+        key: 'index',
 
-header.addEventListener('click', () => {
-    console.log('click');
-    header.style.color = 'green';
-});
+    },
+    // login: {
+    //     name: 'Авторизация',
+    //     href: '/login',
+    //     render: renderLogin,
+    //     key: 'login',
+    // },
+    // signup: {
+    //     name: 'Регистрация',
+    //     href: '/signup',
+    //     render: renderSignup,
+    //     key: 'signup',
+    // },
+    // profile: {
+    //     name: 'Профиль',
+    //     href: '/profile',
+    //     render: renderProfile,
+    //     key: 'profile',
+    // }
+};
 
-add_btn.addEventListener('click', () => {
-    anns.innerHTML += "<div class=\"ann\"></div>"
-})
+function ajax(method, url, body = null, callback) {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url, true);
+    xhr.withCredentials = true;
+
+    xhr.addEventListener('readystatechange', function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+
+        callback(xhr.status, xhr.responseText);
+    });
+
+    if (body) {
+        xhr.setRequestHeader('Content-type', 'application/json; charset=utf8');
+        xhr.send(JSON.stringify(body));
+        return;
+    }
+
+    xhr.send();
+}
+
+function renderBoard(parent) {
+    const boardElement = document.createElement('div');
+
+    ajax(
+        'GET',
+        '/board',
+        null,
+        (status, responseString) => {
+            const anns = JSON.parse(responseString);
+
+            if (anns && Array.isArray(anns)) {
+                const ann_group = document.createElement('div');
+                ann_group.classList.add("ann-group")
+                boardElement.appendChild(ann_group)
+
+                anns.forEach(({src, category, title, price, address}) => {
+                    console.log(src, title);
+                    ann_group.innerHTML += `
+                    <div class="ann">
+                        <div class="ann-img">
+                            <img src="images/${src}" alt="">
+                        </div>
+                        <div class="ann-body">
+                            <div class="ann-category">${category}</div>
+                            <div class="ann-title">${title}</div>
+                            <div class="ann-sale">${price} ₽</div>
+                            <div class="ann-address">${address}</div>
+                        </div>
+                    </div>`
+                })
+            }
+            parent.appendChild(boardElement);
+        }
+    );
+}
+
+function renderModals(parent) {
+    const modal = `
+    <!-- Модальное окно вопроса -->
+    <div id="enterModal" class="modal">
+        <div class="modal-content">
+            <div class="modal-header">
+                <span class="back-btn pointer" id="enterBack"><i class="arrow-left"></i></span>
+                <span class="modal-title">Вход в систему</span>
+                <span class="close-btn pointer" id="enterClose"><i class="close"></i></span>
+            </div>
+            <div class="modal-body">
+              <span class="modal-text text-center">Создайте аккаунт, если ещё не зарегистрированы</span>
+              <button class="btn btn-success grid-center" id="signupBtn">Регистрация</button>
+              <br>
+              <span class="modal-text text-center">Или авторизируйтесь, если уже есть аккаунт</span>
+              <button class="btn btn-primary grid-center" id="signinBtn">Вход</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Модальное окно входа -->
+    <div id="signinModal" class="modal">
+        <div class="modal-content">
+            <div class="modal-header">
+                <span class="back-btn pointer" id="signinBack"><i class="arrow-left"></i></span>
+                <span class="modal-title">Авторизация</span>
+                <span class="close-btn pointer" id="signinClose"><i class="close"></i></span>
+            </div>
+            <div class="modal-body">
+                <form id="sugninForm">
+                    <section class="input-group">
+                        <label class="modal-text" for="username">Имя пользователя</label>
+                        <input type="text" class="input input-error" name="username">
+                        <span class="error-text">Неверное имя пользователя</span>
+                    </section>
+                    <section class="input-group">
+                        <label class="modal-text" for="password">Пароль</label>
+                        <input type="password" class="input" name="password">
+                        <span class="error-text"></span>
+                    </section>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <label class="grid-left modal-text" for="remember">
+                    <input form="signinForm" type="checkbox" name="remember">
+                    Запомнить меня
+                </label>
+                <button class="btn btn-primary grid-right" id="auth-btn">Вход</button>
+            </div>
+        </div>
+    </div>
+
+    <!-- Модальное окно регистрации -->
+    <div id="signupModal" class="modal">
+        <div class="modal-content">
+            <div class="modal-header">
+                <span class="back-btn pointer" id="signupBack"><i class="arrow-left"></i></span>
+                <span class="modal-title">Регистрация</span>
+                <span class="close-btn pointer" id="signupClose"><i class="close"></i></span>
+            </div>
+            <div class="modal-body">
+                <form id="signupForm" action="">
+                    <section class="input-group">
+                        <label class="modal-text" for="username">Имя пользователя</label>
+                        <input type="text" class="input" name="username">
+                        <span class="error-text"></span>
+                    </section>
+                    <section class="input-group">
+                        <label class="modal-text" for="email">Почта</label>
+                        <input type="email" class="input" name="email">
+                        <span class="error-text"></span>
+                    </section>
+                    <section class="input-group">
+                        <label class="modal-text" for="password">Пароль</label>
+                        <input type="password" class="input" name="password">
+                        <span class="error-text"></span>
+                    </section>
+                    <section class="input-group">
+                        <label class="modal-text" for="repeat-password">Повторите пароль</label>
+                        <input type="password" class="input input-error" name="repeat-password">
+                        <span class="error-text">Пароли не совпадают</span>
+                    </section>
+                    <section class="input-group">
+                        <label class="grid-left modal-text" for="accept">
+                            <input type="checkbox" name="accept">
+                            Я принимаю
+                        </label>
+                        <a href="#"><button class="cell-btn-sm">пользовательское соглашение</button></a>
+                    </section>
+                </form>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-primary-tertiary grid-left" id="auth-btn">Есть аккаунт</button>
+                <button form="" class="btn btn-success grid-right" id="auth-btn">Регистрация</button>
+            </div>
+        </div>
+    </div>`;
+    
+    parent.innerHTML += modal;
+
+    // логика
+    const enter_backer = document.getElementById("enterBack");
+    const enter_closer = document.getElementById("enterClose");
+    const signin_btn = document.getElementById("signinBtn");
+    const signup_btn = document.getElementById("signupBtn");
+
+    const signin_backer = document.getElementById("signinBack");
+    const signin_closer = document.getElementById("signinClose");
+
+    const signup_backer = document.getElementById("signupBack");
+    const signup_closer = document.getElementById("signupClose");
+
+    const enter_modal = document.getElementById("enterModal");
+    const signin_modal = document.getElementById("signinModal");
+    const signup_modal = document.getElementById("signupModal");
+
+    enter_backer.onclick = () => {
+        enter_modal.style.display = "none";
+    }
+    enter_closer.onclick = () => {
+        enter_modal.style.display = "none";
+    }
+    signin_btn.onclick = () => {
+        enter_modal.style.display = "none";
+        signin_modal.style.display = "block";
+    }
+    signup_btn.onclick = () => {
+        enter_modal.style.display = "none";
+        signup_modal.style.display = "block";
+    }
+
+    signin_backer.onclick = () => {
+        signin_modal.style.display = "none";
+        enter_modal.style.display = "block";
+    }
+    signin_closer.onclick = () => {
+        signin_modal.style.display = "none";
+    }
+
+    signup_backer.onclick = () => {
+        signup_modal.style.display = "none";
+        enter_modal.style.display = "block";
+    }
+    signup_closer.onclick = () => {
+        signup_modal.style.display = "none";
+    }
+
+    window.onclick = (event) => {
+        if (event.target == enter_modal ||
+            event.target == signin_modal ||
+            event.target == signup_modal) {
+                enter_modal.style.display = "none";
+                signin_modal.style.display = "none";
+                signup_modal.style.display = "none";
+        }
+    } 
+}
+
+function renderHeader(parent) {
+    const nav_brand = document.createElement("div");
+    nav_brand.classList.add("nav-brand");
+    nav_brand.innerText = "AppUniq";
+
+    const category_btn = document.createElement("button");
+    category_btn.classList.add("btn", "btn-primary");
+    category_btn.innerHTML = `<span><i class="play-list"></i></span>Категории`; 
+
+    const search_input = document.createElement("input");
+    search_input.classList.add("input", "search");
+    search_input.placeholder = "Найти товар";
+
+    const add_ann_btn = document.createElement("button");
+    add_ann_btn.classList.add("btn", "btn-success");
+    add_ann_btn.innerHTML = `<span><i class="play-list"></i></span>Разместить объявление`; 
+
+    parent.appendChild(nav_brand);
+    parent.appendChild(category_btn);
+    parent.appendChild(search_input);
+    parent.appendChild(add_ann_btn);
+
+    ajax(
+        'GET',
+        '/me',
+        null,
+        (status, responseString) => {
+            let isAuthorized = status === 200;
+
+            if (isAuthorized) {
+                const user = JSON.parse(responseString);
+                if (!user.avatar) {
+                    user.avatar = "ava.jpeg";
+                }
+
+                const profile = document.createElement("a");
+                profile.classList.add("profile");
+                profile.href = "/profile";
+                profile.innerHTML = `<img src="images/${user.avatar}" alt="" class="avatar">${username}`;
+
+                const logout_btn = document.createElement('button');
+                logout_btn.classList.add("cell-btn-sm", "grid-center");
+                logout_btn.innerHTML = `<i class="log-out"></i>`;
+
+                parent.appendChild(profile);
+                parent.appendChild(logout_btn);
+
+            } else {
+                renderModals(contentElement);
+
+                const enter_btn = document.createElement("button");
+                enter_btn.classList.add("btn", "btn-primary");
+                enter_btn.innerHTML = `<span><i class="chevron-right"></i></span>Войти`;
+
+                parent.appendChild(enter_btn);
+
+                enter_btn.onclick = () => {
+                    const enter_modal = document.getElementById("enterModal");
+                    enter_modal.style.display = "block";
+                }
+            }
+        });
+}
+
+function goToPage(configSection) {
+    contentElement.innerHTML = '';
+    configSection.render(contentElement);
+}
+
+renderHeader(headerElement)
+renderBoard(contentElement);
--- server/index.js
+++ server/index.js
@@ -1,34 +1,122 @@
-const http = require('http');
-const fs = require('fs');
-const debug = require('debug');
+'use strict';
 
-const log = debug('server');
+const express = require('express');
+const body = require('body-parser');
+const cookie = require('cookie-parser');
+const morgan = require('morgan');
+const uuid = require('uuid').v4;
+const path = require('path');
+const app = express();
 
-const SERVER_PORT = 8080;
+const users = {
+    "az.bilalov@mail.ru": {
+        username: "Азат Билалов",
+        email: "az.bilalov@mail.ru",
+        password: "1234",
+        avatar: "/ava1.jpeg",
+        anns: [
+            {src: "/ann1.jpeg", category: "В быту", title: "Тележка", desc: "Удобная тележка", price: "500", address: "Москва, ул. Лобавческого 88"},
+            {src: "/ann2.jpeg", category: "Электрокина", title: "Планшет", desc: "Удобный планшет", price: "15000", address: "Москва, ул. Лобавческого 88"},
+        ]
+    },
+    "test@mail.ru": {
+        username: "Тестер",
+        email: 'test@mail.ru',
+        password: '000',
+    },
+}
 
-const page404 = fs.readFileSync('public/404.html');
+app.use(morgan('dev'));
+app.use(express.static(path.resolve(__dirname, '..', 'public')));
+app.use(express.static(path.resolve(__dirname, 'static')));
+app.use(body.json());
+app.use(cookie());
 
-const server = http.createServer((request, response) => {
-    log('REQUEST', request.method, request.url);
-    const {url} = request;
-    const normalizedUrl = url === '/' ? '/index.html' : url;
+const ids = {};
 
-    log('normalized url ::', normalizedUrl);
-    const filepath = `./public${normalizedUrl}`;
-    log('файл ::', filepath);
-    fs.readFile(filepath, (err, data) => {
-        if (err) {
-            response.write(page404);
-            response.end();
-            log('ошибка ::', err);
-            return;
-        }
+app.post('/signup', (req, res) => {
+	const password = req.body.password;
+	const email = req.body.email;
+	const age = req.body.age;
+	if (
+		!password || !email || !age ||
+		!password.match(/^\S{4,}$/) ||
+		!email.match(/@/)
+	) {
+		return res.status(400).json({error: 'Невалидные данные пользователя'});
+	}
+	if (users[email]) {
+		return res.status(400).json({error: 'Пользователь уже существует'});
+	}
 
-        log('успех');
-        response.write(data);
-        response.end();
-    })
+	const id = uuid();
+	const user = {password, email, age, images: []};
+	ids[id] = email;
+	users[email] = user;
+
+	res.cookie('appuniq', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
+	res.status(201).json({id});
 });
 
-log('Запуск сервера...');
-server.listen(SERVER_PORT);
+app.post('/login',  (req, res) => {
+	const password = req.body.password;
+	const email = req.body.email;
+	if (!password || !email) {
+		return res.status(400).json({error: 'Не указан E-Mail или пароль'});
+	}
+	if (!users[email] || users[email].password !== password) {
+		return res.status(400).json({error: 'Неверный E-Mail и/или пароль'});
+	}
+
+	const id = uuid();
+	ids[id] = email;
+
+	res.cookie('appuniq', id, {expires: new Date(Date.now() + 1000 * 60 * 10)});
+	res.status(200).json({id});
+});
+
+app.get('/profile', (req, res) => {
+	const id = req.cookies['appuniq'];
+	const email = ids[id];
+	if (!email || !users[email]) {
+		return res.status(401).end();
+	}
+
+	res.json(users[email]);
+});
+
+
+app.get('/board', (req, res) => {
+	const id = req.cookies['appuniq'];
+	const emailSession = ids[id];
+
+	const result = Object
+		.values(users)
+		.filter(({email}) => email !== emailSession)
+		.map(user => user.anns)
+		.filter(Boolean);
+
+	res.json(result.flat());
+});
+
+app.get('/me', (req, res) => {
+    const id = req.cookies['appuniq'];
+	const emailSession = ids[id];
+
+    if (!emailSession || !users[emailSession]) {
+		return res.status(401).end();
+	}
+
+    const user = users[emailSession];
+    res.json({
+        username: user.username,
+        email: user.email,
+        avatar: user.avatar,
+    });
+})
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, function () {
+	console.log(`Server listening port ${port}`);
+});
