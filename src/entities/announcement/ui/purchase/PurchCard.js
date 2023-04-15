75860d3b8b7b3d5d384f3b1937fe5589a2a35a61
SicParv1sMagna <92353726+SicParv1sMagna@users.noreply.github.com>
2023-04-15 10:59:16 +0300

    cross svg

--- src/entities/announcement/ui/purchase/PurchCard.js
+++ src/entities/announcement/ui/purchase/PurchCard.js
@@ -1,10 +1,14 @@
 import './PurchCard.scss';
-import template from './PurchCard.handlebars';
+// import template from './PurchCard.handlebars';
+import { Icon } from '@shared/ui';
+import store from "@modules/state-manager";
+import crossSvg from "assets/icons/cross.svg";
 
-export const PurchCard = (parent, config = {id: ""}) => {
+export const PurchCard = (parent, config = { id: "" }) => {
+    const localItemID = config.id;
     config.id += "PurchCard";
     const actions = {};
-    
+
     const self = () => {
         return document.getElementById(config.id);
     }
@@ -30,8 +34,52 @@ export const PurchCard = (parent, config = {id: ""}) => {
     }
 
     const render = () => {
-        parent.insertAdjacentHTML("beforeEnd", template(config));
+        // parent.insertAdjacentHTML("beforeEnd", template(config));
+        const wrapper = document.createElement('div');
+        wrapper.classList.add('purchase-card');
+        parent.appendChild(wrapper);
 
+        const content = document.createElement('div');
+        content.classList.add('purchase-card__content');
+        wrapper.appendChild(content);
+
+        const image = document.createElement('img');
+        image.classList.add('purchase-card__img');
+        image.src = config.src;
+        image.alt = "";
+
+        const tags = document.createElement('div');
+        tags.classList.add('purchase-card__tags');
+        tags.textContent = config.tags;
+
+        const title = document.createElement('div');
+        title.classList.add('purchase-card__title');
+        title.textContent = config.title;
+        console.log(config.title);
+
+        const sale = document.createElement('div');
+        sale.classList.add('purchase-card__sale');
+        sale.textContent = config.price + ' ₽';
+        console.log(config.price);
+
+        const deleteIcon = Icon(wrapper, {
+            id: "deletePurchase",
+            src: crossSvg,
+            size: "large",
+            direction: "row",
+            actions: {
+                'click' : () => {
+                    localStorage.removeItem(localItemID);
+                    store.setState('bucket', 'clearOne');
+                }
+            }
+        });
+
+        content.appendChild(image);
+        content.appendChild(tags);
+        content.appendChild(title);
+        content.appendChild(sale);
+        deleteIcon.render();
         applyActions();
     }
 
--- src/pages/bucket/index.js
+++ src/pages/bucket/index.js
@@ -45,6 +45,23 @@ export const bucketPage = (parent) => {
                 priceWidget.destroy();
                 priceWidget.render();
             }
+            if (newState === 'clearOne') {
+                purchGroup.innerHTML = '';
+                priceWidget.destroy();
+                priceWidget.render();
+                Object.keys(localStorage).forEach(function(key) {
+                    const obj = JSON.parse(localStorage.getItem(key))
+                    const purchCard = PurchCard(purchGroup, {
+                        id: obj.id,
+                        tags: obj.tags,
+                        title: obj.title,
+                        price: obj.price,
+                        address: obj.address,
+                        src: obj.images[0],
+                    });
+                    purchCard.render();
+                });
+            }
         });
 
         Object.keys(localStorage).forEach(function(key) {
